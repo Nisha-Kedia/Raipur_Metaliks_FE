791f1f5f263a81c856fb677b07bca15c5a26906b
Nisha-Kedia <114925494+Nisha-Kedia@users.noreply.github.com>
2025-07-22 01:23:31 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -33,7 +33,7 @@ function Contact() {
         phoneNo: formData.phoneNo ? parseInt(formData.phoneNo, 10) : null
       };
       
-      const response = await fetch('http://raipurmetaliksbackend-production.up.railway.app/api/contacts', {
+      const response = await fetch('http://raipurmetaliksbe-production.up.railway.app:8080/api/contacts', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
